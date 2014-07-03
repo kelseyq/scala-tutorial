63f0fd1cdcd72f91bc2ac03947d4ddec767efcbf
Kelsey Gilmore-Innis <kelsey@helloreverb.com>
2014-07-03 13:04:18 -0700

    reupgrade sbt

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,1 +1,3 @@
 addSbtPlugin("org.scalatra.sbt" % "scalatra-sbt" % "0.3.5")
+
+addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
--- src/main/scala/scalatutorial/app/service/WeatherService.scala
+++ src/main/scala/scalatutorial/app/service/WeatherService.scala
@@ -1,29 +1,28 @@
 package scalatutorial.app.service
 
 import java.io.File
-import org.joda.time.DateTime
 import java.util.Date
 import java.text.SimpleDateFormat
 
 object WeatherService {
- lazy val weather = scala.io.Source.fromURL(getClass.getResource("/weather.csv"))
+  lazy val weather = scala.io.Source.fromURL(getClass.getResource("/weather.csv"))
 
   val dateFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
 
-  def getRecords(): List[WeatherRecord] = try {
-  val weatherRecords = weather.getLines().toList.map{ line =>
+  def getRecords(): List[WeatherRecord] = {
+    val weatherRecords = weather.getLines().toList.map{ line =>
 
-    val entries = line.split(",").map{ field => field.trim }
+      val entries = line.split(",").map{ field => field.trim }
 
-    val entry_city = entries(0)
-    val entry_date = dateFormat.parse(entries(1))
-    val entry_high = entries(2).toInt
-    val entry_low = entries(3).toInt
+      val entry_city = entries(0)
+      val entry_date = dateFormat.parse(entries(1))
+      val entry_high = entries(2).toInt
+      val entry_low = entries(3).toInt
 
-    WeatherRecord(entry_city, entry_date, entry_high, entry_low)
+      WeatherRecord(entry_city, entry_date, entry_high, entry_low)}
+    weather.close
+    weatherRecords
   }
-   weather.close
-   weatherRecords
 }
 
 case class WeatherRecord(city: String, date: Date, high: Int, low: Int)
