f0b5b3f5570a39dcaf21683d85a020c0201ace44
Kelsey Gilmore-Innis <kelsey@helloreverb.com>
2014-07-12 19:47:46 -0700

    add answers

--- src/main/scala/scalatutorial/app/TutorialApp.scala
+++ src/main/scala/scalatutorial/app/TutorialApp.scala
@@ -1,27 +1,57 @@
 package scalatutorial.app
 
 import org.scalatra._
-import scalatutorial.app.service.WeatherService
+import scalatutorial.app.service.{WeatherRecord, WeatherService}
 import scalatutorial.app.utils.TutorialServlet
 
 class TutorialApp extends TutorialServlet {
 
+  /*
+   * Big thanks to Tony (https://github.com/t-pleasure) for contributing the answers!
+   */
+
+  //REPL work
+  //TASKS:
+  //  Store your own name as a value
+      //  scala> val myName = "Tony"
+      //  myName: String = Tony
+
+  //  Store the name of the person sitting next to you as a value
+      //  scala> val neighbor = "Kelsey"
+      //  neighbor: String = Kelsey
+
+  //  Store your names + your neighbors' names in a list
+      //  scala> val listOfNeighbors = List("Tony", "Kelsey", "Jason")
+      //  listOfNeighbors: List[String] = List(Tony, Kelsey, Jason)
+
+  //  Ask two people sitting near you what age they started programming
+  //  Find the average age that all of you started programming, without rounding
+      //  scala> val averageAge: Double = (12 + 21 + 16)/3.0
+      //  averageAge: Double = 16.333333333333332
+
+  //  Create a map of name to age
+      //  scala> val mapOfNeighbors = Map("Tony" -> 12, "Kelsey" -> 21, "Jason" -> 16)
+      //  mapOfNeighbors: scala.collection.immutable.Map[String,Int] = Map(Tony -> 12, Kelsey -> 21, Jason -> 16)
+
   //SETUP
+  //TASK: change "Hello OSCON" to hello something else
   get("/") {
-    "Hello OSCON!"
+    "Hello completed answers!"
   }
 
   //EXPRESSIONS
+  //TASK: Modify the /smalltalk endpoint code to operate on an integer representing the temperature
   get("/smalltalk") {
-    val weather = params("weather")
+    val weather = params("weather").toInt
 
-    if (weather == "hot") {
+    if (weather > 70) {
       "Hot enough for ya?"
     } else {
       "Brrrr!"
     }
   }
 
+  //TASK: Add a default to /moreSmalltalk that returns "Yup, sure is FOO"
   get("/moreSmalltalk") {
     val weather = params("weather")
 
@@ -30,45 +60,77 @@ class TutorialApp extends TutorialServlet {
       case "nice" => "Couldn't ask for a nicer day, huh?"
       case "rain" => "It's coming down cats & dogs"
       case "cold" => "It's colder than a polar bear's toenails out there!"
+      case other => "Yup, sure is " + other
     }
   }
 
   /*
    * http://localhost:8080/highlow?temps=45,70 should return
-   * "The high temperature is 45 and the low temperature is 70"
+   * "The low temperature is 45 and the high temperature is 70"
+   *
+   * TASK: Implement/highLow
+   * Use pattern matching with value binding to extract the high & low temp
+   * Use guards to validate the data
    */
   get("/highLow") {
-    val temps: List[String] = multiParams("temps").toList
+    val temps: List[Int] = getParamsList(params("temps"))
 
-    ???
+    // safe guard to ensure that we know which value is the high and which is the low.
+    temps match {
+      case List(low, high) if high >= low => "The low temperature is " + low + " and the high temperature is " + high
+      case _ => "High and low temperature not found"
+    }
   }
 
   //FUNCTIONS
-  def fToC(fahrenheit: Int): Double = {
+  def fToC(fahrenheit: Double): Double = {
     (fahrenheit - 32.0) * (5.0/9.0)
   }
 
   get("/convertToC") {
-    val f = params("temp").toInt
+    val f = params("temp").toDouble
     fToC(f)
   }
 
-  def cToF(celsius: Int): Double = ???
+  def cToF(celsius: Double): Double = {
+    (celsius * 9.0/5.0) + 32
+  }
 
+  //TASK: Complete the convertToF endpoint
   get("/convertToF") {
-    ???
+    val f = params("temp").toDouble
+    cToF(f)
+  }
+
+  //TASK: Refactor your code from the /smalltalk endpoint to call a function
+  // get("/smalltalk") can now just call getSmalltalk(weather)
+  def getSmalltalk(weather: Int): String = {
+    if (weather > 70) {
+      "Hot enough for ya?"
+    } else {
+      "Brrrr!"
+    }
   }
 
   //CASE CLASS
-  case class Cloud(name: String, heightInFeet: Int)
+  //TASK: Add a field of your choosing to the Cloud case class
+  //TASK: Create an object outside of TutorialApp with a convertFeetToMeters method
+  //   See below
+
+  case class Cloud(name: String, heightInFeet: Int, looksLike: String) {
+    //TASK: Add a method to Cloud that uses the object's method to return the cloud's height in meters
+    def meters = Converter.convertFeetToMeters(heightInFeet)
+  }
 
   get("/clouds") {
-    val cirrus = Cloud("cirrus", 18000)
-    val altostratus = Cloud("altostratus", 10000)
-    val cumulus = Cloud("cumulus", 6000)
+    val cirrus = Cloud("cirrus", 18000, "a bunny")
+    val altostratus = Cloud("altostratus", 10000, "Richard Nixon")
+    val cumulus = Cloud("cumulus", 6000, "South America")
 
     val allClouds = List(cirrus, altostratus, cumulus)
-    allClouds
+
+   //TASK:  Change /clouds to print out all the cloud names in uppercase using .map
+    allClouds.map(_.name.toUpperCase)
   }
 
   get("/weatherRecords") {
@@ -76,15 +138,60 @@ class TutorialApp extends TutorialServlet {
   }
 
   //FIRST CLASS FUNCTIONS
+  // TASK: Implement /averageTemp:city
   get("/averageTemp") {
-    val date = params("city")
+    val city = params("city")
+    // obtain records for the specified city
+    val records =  WeatherService.getRecords.filter{r: WeatherRecord => r.city == city}
+    // obtain the number of records associated with the city
+    val nRecords = records.length.toDouble
+    // compute average high and low temperatures
+    val averageHigh = records.map{r: WeatherRecord => r.high}.sum / nRecords
+    val averageLow = records.map{r: WeatherRecord => r.low}.sum / nRecords
 
-    ???
+    Map("average_high"->averageHigh, "average_low"->averageLow)
   }
 
+  /**
+   * TASK: Implement /maxTemp (maximum temperature for a given date)
+   */
   get("/maxTemp") {
+    // obtain date input
     val date = params("date")
-    ???
+    // parse the date input into an actual Date class
+    val dateObj = WeatherService.dateFormat.parse(date)
+    // obtain records that match the target date
+    val records = WeatherService.getRecords.filter{r: WeatherRecord => r.date.equals(dateObj)}
+
+    // return the record with the maximum temperature by finding the record with the largest high temp.
+    // (note: we can ignore low temps)
+    records.maxBy{r: WeatherRecord => r.high}
   }
 
+  /**
+   * TASK: Come up with a query of your choice on the weather data and implement an end point for it.
+   * NOTE: Here we are implementing the /stdTemp end point which computes the standard deviation of
+   * high and low temperatures.
+   */
+  def std(xs: List[Int]): Double = {
+    val n = xs.length.toDouble
+    val mu = xs.sum / n
+    Math.sqrt((xs.map(x => (x-mu)*(x-mu)).sum)/n)
+  }
+
+  get("/stdTempDev") {
+    val city = params("city")
+    // obtain records for the specified city
+    val records = WeatherService.getRecords.filter{r: WeatherRecord => r.city == city}
+    // obtain high temperatures and low temperatures of records
+    val highs = records.map{r: WeatherRecord => r.high}
+    val lows = records.map{r: WeatherRecord => r.low}
+
+    Map("std of lows"->std(lows), "std of highs"->std(highs))
+  }
+}
+
+//TASK: Create an object outside of TutorialApp with a convertFeetToMeters method
+object Converter {
+  def convertFeetToMeters(feet: Int): Double = feet * 0.3048
 }
--- src/main/scala/scalatutorial/app/service/WeatherService.scala
+++ src/main/scala/scalatutorial/app/service/WeatherService.scala
@@ -5,11 +5,11 @@ import java.util.Date
 import java.text.SimpleDateFormat
 
 object WeatherService {
-  lazy val weather = scala.io.Source.fromURL(getClass.getResource("/weather.csv"))
-
   val dateFormat: SimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd")
 
   def getRecords(): List[WeatherRecord] = {
+    lazy val weather = scala.io.Source.fromURL(getClass.getResource("/weather.csv"))
+
     val weatherRecords = weather.getLines().toList.map{ line =>
 
       val entries = line.split(",").map{ field => field.trim }
@@ -19,10 +19,18 @@ object WeatherService {
       val entry_high = entries(2).toInt
       val entry_low = entries(3).toInt
 
-      WeatherRecord(entry_city, entry_date, entry_high, entry_low)}
+      /*
+       * TASK: Modify WeatherService to handle both csv entries with rainfall field and without
+       * Note: I'm going to assume that csv rows with 5 elements will have their last element
+       * represent rainfall data. Also, for rows with no rainfall data, we will create WeatherRecords
+       * with -1 assigned to rainfallInInches.
+       */
+      val entry_rainfallInInches = if(entries.length == 5) entries(4).toDouble else -1.0
+
+      WeatherRecord(entry_city, entry_date, entry_high, entry_low, entry_rainfallInInches)}
     weather.close
     weatherRecords
   }
 }
 
-case class WeatherRecord(city: String, date: Date, high: Int, low: Int)
+case class WeatherRecord(city: String, date: Date, high: Int, low: Int, rainfall: Double)
