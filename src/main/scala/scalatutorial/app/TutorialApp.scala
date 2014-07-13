1118a26fc402a6b0db5c5ecbb0bf7a28ae919419
Kelsey Gilmore-Innis <kelsey@helloreverb.com>
2014-07-12 18:12:51 -0700

    clean up sample project

---
+++ src/main/scala/scalatutorial/app/TutorialApp.scala
@@ -0,0 +1,90 @@
+package scalatutorial.app
+
+import org.scalatra._
+import scalatutorial.app.service.WeatherService
+import scalatutorial.app.utils.TutorialServlet
+
+class TutorialApp extends TutorialServlet {
+
+  //SETUP
+  get("/") {
+    "Hello OSCON!"
+  }
+
+  //EXPRESSIONS
+  get("/smalltalk") {
+    val weather = params("weather")
+
+    if (weather == "hot") {
+      "Hot enough for ya?"
+    } else {
+      "Brrrr!"
+    }
+  }
+
+  get("/moreSmalltalk") {
+    val weather = params("weather")
+
+    weather match {
+      case "hot" => "Don't forget the sunscreen!"
+      case "nice" => "Couldn't ask for a nicer day, huh?"
+      case "rain" => "It's coming down cats & dogs"
+      case "cold" => "It's colder than a polar bear's toenails out there!"
+    }
+  }
+
+  /*
+   * http://localhost:8080/highlow?temps=45,70 should return
+   * "The high temperature is 45 and the low temperature is 70"
+   */
+  get("/highLow") {
+    val temps: List[String] = multiParams("temps").toList
+
+    ???
+  }
+
+  //FUNCTIONS
+  def fToC(fahrenheit: Int): Double = {
+    (fahrenheit - 32.0) * (5.0/9.0)
+  }
+
+  get("/convertToC") {
+    val f = params("temp").toInt
+    fToC(f)
+  }
+
+  def cToF(celsius: Int): Double = ???
+
+  get("/convertToF") {
+    ???
+  }
+
+  //CASE CLASS
+  case class Cloud(name: String, heightInFeet: Int)
+
+  get("/clouds") {
+    val cirrus = Cloud("cirrus", 18000)
+    val altostratus = Cloud("altostratus", 10000)
+    val cumulus = Cloud("cumulus", 6000)
+
+    val allClouds = List(cirrus, altostratus, cumulus)
+    allClouds
+  }
+
+  get("/weatherRecords") {
+    WeatherService.getRecords
+  }
+
+  //FIRST CLASS FUNCTIONS
+  get("/averageTemp") {
+    val date = params("city")
+
+    ???
+  }
+
+  get("/maxTemp") {
+    val date = params("date")
+    ???
+  }
+
+}
---
+++ src/main/scala/scalatutorial/app/utils/TutorialServlet.scala
@@ -0,0 +1,15 @@
+package scalatutorial.app.utils
+
+import org.scalatra._
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
+
+trait TutorialServlet extends ScalatraServlet with JacksonJsonSupport {
+
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
+
+}
